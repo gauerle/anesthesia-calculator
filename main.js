47d9df56c1432a8e6ad3508080cc66ad6c72e573
gauerle <gauerle@gmail.com>
2025-09-08 18:20:30 -0300

    Add Electron build configuration

---
+++ main.js
@@ -0,0 +1,17 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true
+    }
+  });
+  
+  win.loadFile('index.html'); // Your HTML file
+}
+
+app.whenReady().then(createWindow);
